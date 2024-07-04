48dc9964cededf7d9accccda328ec6c97cc50a78
TorrecillaER <aaron@energiareal.mx>
2024-07-04 16:55:41 -0600

    feat: Adding validations on publish que

--- er.huawei.replicador.func/Domain/CommandHandlers/RealTimeDataCommandHandler.cs
+++ er.huawei.replicador.func/Domain/CommandHandlers/RealTimeDataCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using er.huawei.replicador.func.Domain.Commands;
+﻿using er.huawei.replicador.func.Application.Model.Dto;
+using er.huawei.replicador.func.Domain.Commands;
 using ER.Huawei.Replicador.Core.Bus;
 using MediatR;
 
@@ -14,12 +15,35 @@ public class RealTimeDataCommandHandler : IRequestHandler<RealTimeDataCommand, b
         _bus = bus;
     }
 
-    public Task<bool> Handle(RealTimeDataCommand request, CancellationToken cancellationToken)
+    public async Task<bool> Handle(RealTimeDataCommand request, CancellationToken cancellationToken)
     {
         //logica para publicar el mensaje dentro del event bus rabbitmq
+        var plantDeviceResult = request.PlantDeviceResult;
 
-        _bus.Publish(request.PlantDeviceResult);
+        if ((plantDeviceResult.invertersList == null || !plantDeviceResult.invertersList.Any()) &&
+        (plantDeviceResult.metterList == null || !plantDeviceResult.metterList.Any()))
+        {
+            plantDeviceResult.invertersList = new List<DeviceDataResponse<DeviceInverterDataItem>>();
+            plantDeviceResult.metterList = new List<DeviceDataResponse<DeviceMetterDataItem>>();
+            _bus.Publish(plantDeviceResult);
+            return await Task.FromResult(true);
+        }
+        bool hasValidInverterData = plantDeviceResult.invertersList != null &&
+                               plantDeviceResult.invertersList.Any(i => i.dataItemMap != null && i.dataItemMap.total_cap != null);
 
-        return Task.FromResult(true);
+        bool hasValidMetterData = plantDeviceResult.metterList != null &&
+                                  plantDeviceResult.metterList.Any(m => m.dataItemMap != null && m.dataItemMap.reverse_active_cap != null);
+
+        if (!hasValidInverterData)
+        {
+            plantDeviceResult.invertersList = new List<DeviceDataResponse<DeviceInverterDataItem>>();
+        }
+        if (!hasValidMetterData)
+        {
+            plantDeviceResult.metterList = new List<DeviceDataResponse<DeviceMetterDataItem>>();
+        }
+        else
+        _bus.Publish(plantDeviceResult);
+        return await Task.FromResult(true);
     }
 }
