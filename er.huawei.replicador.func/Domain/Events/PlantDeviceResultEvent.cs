e08fcb5ebc104a5ea43df98bbc7a65ece57af000
Mario Enrique Chan Fernandez <mario@energiareal.mx>
2024-07-04 12:36:02 -0600

    Update PlantDeviceResultEvent.cs

--- er.huawei.replicador.func/Domain/Events/PlantDeviceResultEvent.cs
+++ er.huawei.replicador.func/Domain/Events/PlantDeviceResultEvent.cs
@@ -4,7 +4,6 @@ using ER.Huawei.Replicador.Core.Events;
 using MongoDB.Bson;
 public class PlantDeviceResultEvent : Event
 {
-    public ObjectId _id { get; set; }
     public string brandName { get; set; } = "brand";
     public string stationCode { get; set; }
     public DateTime repliedDateTime { get; set; }
